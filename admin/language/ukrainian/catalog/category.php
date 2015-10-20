14e992017c21af61e68ae47443c903335b90f4bf
fradee <fradee@gmail.com>
2015-10-20 16:21:58 +0300

    Зміни для здійснення: 	змінено:                    admin/language/ukrainian/catalog/category.php 	admin/language/ukrainian/catalog/category.php:1 - не зрозуміла крапка на початку файла
    	змінено:                    admin/language/ukrainian/user/api.php
    	admin/language/ukrainian/user/api.php:29 - після ; текст

--- admin/language/ukrainian/catalog/category.php
+++ admin/language/ukrainian/catalog/category.php
@@ -1,8 +1,4 @@
-﻿<?php
-
-//version 2.0.0.0
-//Made by Sirchyk for www.marketplus.if.ua on 16 of october 2014.
-//info@marketplus.if.ua
+<?php
 
 // Heading
 $_['heading_title']          = 'Категорії';
