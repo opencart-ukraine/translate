8cb8b5d6641d8a1bcf6ea09bc75dac2cf0a74479
fradee <fradee@gmail.com>
2015-10-22 04:10:51 +0300

    Зміни для здійснення: 	new file:                   admin/language/ukrainian/analytics/google_analytics.php 	new file:                   admin/language/ukrainian/captcha/basic_captcha.php 	new file:                   admin/language/ukrainian/captcha/google_captcha.php 	new file:                   admin/language/ukrainian/customer/custom_field.php 	new file:                   admin/language/ukrainian/customer/customer.php 	new file:                   admin/language/ukrainian/customer/customer_ban_ip.php 	new file:                   admin/language/ukrainian/customer/customer_group.php 	new file:                   admin/language/ukrainian/extension/analytics.php 	new file:                   admin/language/ukrainian/extension/captcha.php 	new file:                   admin/language/ukrainian/fraud/fraudlabspro.php 	new file:                   admin/language/ukrainian/fraud/ip.php 	new file:                   admin/language/ukrainian/fraud/maxmind.php

---
+++ admin/language/ukrainian/extension/captcha.php
@@ -0,0 +1,20 @@
+<?php
+
+//version 2.1.0.x
+//Translated by fradee (Oleksandr Fradynsky) with love to Ukraine and OpenCart at 21 October 2015
+//fradee@gmail.com
+
+// Heading
+$_['heading_title']    = 'Captcha';
+
+// Text
+$_['text_success']     = 'Успішно змінено Сaptcha!';
+$_['text_list']        = 'Перелік Captcha';
+
+// Column
+$_['column_name']      = 'Назва Captcha';
+$_['column_status']    = 'Статус';
+$_['column_action']    = 'Дія';
+
+// Error
+$_['error_permission'] = 'Увага: відсутні права на редагування Сaptcha!';
---
+++ admin/language/ukrainian/module/amazon_login.php
@@ -0,0 +1,38 @@
+<?php
+
+//version 2.1.0.x
+//Translated by fradee (Oleksandr Fradynsky) with love to Ukraine and OpenCart at 21 October 2015
+//fradee@gmail.com
+
+// Heading
+$_['heading_title'] = 'Авторизація через Amazon';
+
+//Text
+$_['text_module'] = 'Модулі';
+$_['text_success'] = 'Успішно змінено Авторизація через Amazon!';
+$_['text_content_top'] = 'Над контентом';
+$_['text_content_bottom'] = 'Нижче контенту';
+$_['text_column_left'] = 'Ліва колонка';
+$_['text_column_right'] = 'Права колонка';
+$_['text_lwa_button'] = 'Авторизація через Amazon';
+$_['text_login_button'] = 'Логін';
+$_['text_a_button'] = 'A';
+$_['text_gold_button'] = 'Золотий';
+$_['text_darkgray_button'] = 'Темно-сірий';
+$_['text_lightgray_button'] = 'Світло-сірий';
+$_['text_small_button'] = 'Малий';
+$_['text_medium_button'] = 'Середній';
+$_['text_large_button'] = 'Великий';
+$_['text_x_large_button'] = 'Велетенський';
+
+//Entry
+$_['entry_button_type'] = 'Тип кнопки';
+$_['entry_button_colour'] = 'Колір кнопки';
+$_['entry_button_size'] = 'Розмір кнопки';
+$_['entry_layout'] = 'Шаблон';
+$_['entry_position'] = 'Розміщення';
+$_['entry_status'] = 'Статус';
+$_['entry_sort_order'] = 'Порядок сортування';
+
+//Error
+$_['error_permission'] = 'Увага: відсутній дозвіл на редагування модулю Авторизація через Amazon!';
