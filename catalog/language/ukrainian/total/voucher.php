e22dd257bd7642e0d8992e6788ccaa728339a26b
dinox-pro <webmaster@webplaneta.com.ua>
2017-03-01 15:06:59 +0200

    Видалення старої структури перекладу

--- catalog/language/ukrainian/total/voucher.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//version 2.0.0.0
-//Made by Sirchyk for www.marketplus.if.ua on 16 of october 2014.
-//info@marketplus.if.ua
-
-// Text
-$_['text_voucher'] = 'Подарунковий сертифікат (%s)';
