6d3ae626299014fb30eae1522537859236eba44a
ANT <velmyshanovnyi@gmail.com>
2017-06-07 18:47:19 +0300

    * 100% contact

--- system/storage/modification/catalog/language/uk-ua/information/contact.php
+++ system/storage/modification/catalog/language/uk-ua/information/contact.php
@@ -1,29 +1,34 @@
 <?php
+
+// translate for OpenCart 2.3.x by OpenCart Ukrainian Community http://opencart.ua
+// Made by ANT velmyshanovnyi@gmail.com | https://github.com/velmyshanovnyi
+// Made by ANT for https://github.com/opencart-ukraine/translate
+// version 2.3.x
+
 // Heading
-$_['heading_title']  = 'Contact Us';
+$_['heading_title']  = 'Контакти';
 
 // Text
-$_['text_location']  = 'Our Location';
-$_['text_store']     = 'Our Stores';
-$_['text_contact']   = 'Contact Form';
-$_['text_address']   = 'Address';
-$_['text_telephone'] = 'Telephone';
-$_['text_fax']       = 'Fax';
-
-				$_['text_open']      = 'Opening Hours';
-				
-$_['text_comment']   = 'Comments';
-$_['text_success']   = '<p>Your enquiry has been successfully sent to the store owner!</p>';
+$_['text_location']  = 'Наше розміщення';
+$_['text_store']     = 'Наш магазин';
+$_['text_contact']   = 'Форма зв`язку';
+$_['text_address']   = 'Адреса';
+$_['text_telephone'] = 'Телефон';
+$_['text_fax']       = 'Факс';
+$_['text_open']      = 'Графік роботи';
+$_['text_comment']   = 'Коментарі';
+$_['text_success']   = '<p>Ваш запит було успішно надіслано адміністрації магазину!</p>';
 
 // Entry
-$_['entry_name']     = 'Your Name';
-$_['entry_email']    = 'E-Mail Address';
-$_['entry_enquiry']  = 'Enquiry';
+$_['entry_name']     = 'Ваше ім`я';
+$_['entry_email']    = 'E-Mail';
+$_['entry_enquiry']  = 'Запит';
+$_['entry_captcha']  = 'Введіь код в полі нижче';
 
 // Email
-$_['email_subject']  = 'Enquiry %s';
+$_['email_subject']  = 'Запит %s';
 
 // Errors
-$_['error_name']     = 'Name must be between 3 and 32 characters!';
-$_['error_email']    = 'E-Mail Address does not appear to be valid!';
-$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
+$_['error_name']     = 'Ім`я повинно містити від 3 до 32 символів!';
+$_['error_email']    = 'Неправильний E-Mail!'; // 'E-Mail Address does not appear to be valid!'
+$_['error_enquiry']  = 'Запит повинен містити від 10 до 3000 символів!';
