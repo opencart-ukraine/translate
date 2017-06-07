83b7af28a77082ae6dc4efd4049486d04aed14b5
ANT <velmyshanovnyi@gmail.com>
2017-06-07 18:18:37 +0300

    * 100% search

--- system/storage/modification/catalog/language/uk-ua/common/search.php
+++ system/storage/modification/catalog/language/uk-ua/common/search.php
@@ -1,4 +1,10 @@
 <?php
+
+// translate for OpenCart 2.3.x by OpenCart Ukrainian Community http://opencart.ua
+// Made by ANT velmyshanovnyi@gmail.com | https://github.com/velmyshanovnyi
+// Made by ANT for https://github.com/opencart-ukraine/translate
+// version 2.3.x
+
 // Text
- $_['text_placeholder'] = 'Find anything for your home'; 
-$_['text_search'] = 'Search';
+$_['text_placeholder'] = 'Знайти щось для вашого будинку'; // 'Find anything for your home'
+$_['text_search'] = 'Пошук';
