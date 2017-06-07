0e617a365dc872b332ec609777871d51169a7cc7
ANT <velmyshanovnyi@gmail.com>
2017-06-07 19:54:32 +0300

    * 100% product

--- system/storage/modification/catalog/language/uk-ua/product/product.php
+++ system/storage/modification/catalog/language/uk-ua/product/product.php
@@ -1,68 +1,64 @@
 <?php
+
+// translate for OpenCart 2.3.x by OpenCart Ukrainian Community http://opencart.ua
+// Made by ANT velmyshanovnyi@gmail.com | https://github.com/velmyshanovnyi
+// Made by ANT for https://github.com/opencart-ukraine/translate
+// version 2.3.x
+
 // Text
-$_['text_search']              = 'Search';
-
-				$_['text_sale']      = 'Sale';
-				$_['text_new'] = 'New';
-				
-
-				$_['text_quick'] = 'Quick View';
-				
-$_['text_brand']               = 'Brand';
-$_['text_manufacturer']        = 'Brand:';
-$_['text_model']               = 'Product Code:';
-$_['text_reward']              = 'Reward Points:';
-$_['text_points']              = 'Price in reward points:';
-$_['text_stock']               = 'Availability:';
-$_['text_instock']             = 'In Stock';
-
-				$_['text_option']              = 'Available Options';
-				$_['text_select']              = '--- Please Select ---';
-				
-$_['text_tax']                 = 'Ex Tax:';
-$_['text_discount']            = ' or more ';
-$_['text_option']              = 'Available Options';
-$_['text_minimum']             = 'This product has a minimum quantity of %s';
-$_['text_reviews']             =  '%s Review(s)' ;
-$_['text_write']               = 'Write a review';
-$_['text_login']               = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
-$_['text_no_reviews']          = 'There are no reviews for this product.';
-$_['text_note']                = '<span class="text-danger">Note:</span> HTML is not translated!';
-$_['text_success']             = 'Thank you for your review. It has been submitted to the webmaster for approval.';
-$_['text_related']             =  'Other products' ;
-$_['text_tags']                = 'Tags:';
-$_['text_error']               = 'Product not found!';
-$_['text_payment_recurring']   = 'Payment Profile';
-$_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
-$_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
-$_['text_payment_cancel']      = '%s every %d %s(s) until canceled';
-$_['text_day']                 = 'day';
-$_['text_week']                = 'week';
-$_['text_semi_month']          = 'half-month';
-$_['text_month']               = 'month';
-$_['text_year']                = 'year';
+$_['text_search']              = 'Пошук';
+$_['text_sale']                = 'Знижка';
+$_['text_new']                 = 'Новий';
+$_['text_quick']               = 'Швидкий перегляд';
+$_['text_brand']               = 'Бренд';
+$_['text_manufacturer']        = 'Виробник:';
+$_['text_model']               = 'Артикул:';
+$_['text_reward']              = 'Бонусні бали:';
+$_['text_points']              = 'Ціна в бонусних балах:';
+$_['text_stock']               = 'Наявність:';
+$_['text_instock']             = 'В наявності';
+$_['text_option']              = 'Наявні опції'; // 'Available Options' 
+$_['text_select']              = '--- Оберіть ---';
+$_['text_tax']                 = 'Без ПДВ:';
+$_['text_discount']            = ' чи більше ';
+$_['text_option']              = 'Доступні опції';
+$_['text_minimum']             = 'Мінімальне замовлення цього товару %s';
+$_['text_reviews']             = '%s відгуків';
+$_['text_write']               = 'Написати відгук';
+$_['text_login']               = 'Будь-ласка <a href="%s">ввійдіть</a> чи <a href="%s">зареєструйтесь</a> для написання відгуку';
+$_['text_no_reviews']          = 'В цього товару немає відгуків.';
+$_['text_note']                = '<span class="text-danger">Зверніть увагу:</span> HTML не перекладено!';
+$_['text_success']             = 'Дякуємо з Ваш відгук. Він направлений для затвердження модератору..';
+$_['text_related']             = 'Схожі товари' ;
+$_['text_tags']                = 'Теги:';
+$_['text_error']               = 'Товар не знайдено!';
+$_['text_payment_recurring']   = 'Профілі оплати';
+$_['text_trial_description']   = '%s кожен %d %s(s) до %d платежу(ів) тоді';
+$_['text_payment_description'] = '%s кожен %d %s(s) до %d патежу(ів)';
+$_['text_payment_cancel']      = '%s кожен %d %s(s) поки не скасовано';
+$_['text_day']                 = 'день';
+$_['text_week']                = 'тиждень';
+$_['text_semi_month']          = 'половина місяця';
+$_['text_month']               = 'місяць';
+$_['text_year']                = 'рік';
 
 // Entry
-$_['entry_qty']                =  'Quantity' ;
-$_['entry_name']               = 'Your Name';
-$_['entry_review']             = 'Your Review';
-$_['entry_rating']             = 'Rating';
-$_['entry_good']               = 'Good';
-$_['entry_bad']                = 'Bad';
+$_['entry_qty']                = 'Кількість' ;
+$_['entry_name']               = 'Ваше Ім`я';
+$_['entry_review']             = 'Ваш відгук';
+$_['entry_rating']             = 'Рейтинг';
+$_['entry_good']               = 'Хороший';
+$_['entry_bad']                = 'Поганий';
 
 // Tabs
-$_['tab_description']          = 'Description';
-$_['tab_attribute']            =  'Additional Info' ;
- 
-				$_['tab_video'] = 'Video';
-				
- 
-				$_['text_rating'] = 'Overall Rating';
-				$_['text_write1'] = 'Add your review'; 
-				
-$_['tab_review']               =  'Reviews' ;
+$_['tab_description']          = 'Опис';
+$_['tab_attribute']            = 'Специфікація' ;
+$_['tab_video']                = 'Відео';
+$_['text_rating']              = 'Загальний рейтинг';
+$_['text_write1']              = 'Написати відгук'; 
+$_['tab_review']               = 'Відгуки' ;        // 'Reviews' / 'Відгуки (%s)'
 
 // Error
-$_['error_name']               = 'Warning: Review Name must be between 3 and 25 characters!';
-$_['error_text']               = 'Warning: Review Text must be between 25 and 1000 characters!';
-$_['error_rating']             = 'Warning: Please select a review rating!';
+$_['error_name']               = 'Назва відгуку повинна містити від 3 до 25 символів!';
+$_['error_text']               = 'Текст відгуку повинен містити від 25 до 1000 символів!';
+$_['error_rating']             = 'Оберіть ретинг відгуку!';
