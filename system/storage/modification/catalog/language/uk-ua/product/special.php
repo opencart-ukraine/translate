8d5ff1209248c4f85ea8dff9c13b55e2b9d45119
ANT <velmyshanovnyi@gmail.com>
2017-06-07 20:10:35 +0300

    * 100% special

--- system/storage/modification/catalog/language/uk-ua/product/special.php
+++ system/storage/modification/catalog/language/uk-ua/product/special.php
@@ -1,38 +1,36 @@
 <?php
+
+// translate for OpenCart 2.3.x by OpenCart Ukrainian Community http://opencart.ua
+// Made by ANT velmyshanovnyi@gmail.com | https://github.com/velmyshanovnyi
+// Made by ANT for https://github.com/opencart-ukraine/translate
+// version 2.3.x
+
 // Heading
-$_['heading_title']     = 'Special Offers';
+$_['heading_title']     = 'Спеціальні пропозиції';
 
 // Text
-$_['text_empty']        = 'There are no special offer products to list.';
-
-				$_['text_sale']      = 'Sale';
-				$_['text_new'] = 'New';
-				
-
-				$_['text_quick'] = 'Quick View';
-				
-
-				$_['text_load_more'] = 'Load %s more products';
-				
-$_['text_quantity']     = 'Qty:';
-$_['text_manufacturer'] = 'Brand:';
-$_['text_model']        = 'Product Code:';
-$_['text_points']       = 'Reward Points:';
-$_['text_price']        = 'Price:';
-
-				$_['text_option']              = 'Available Options';
-				$_['text_select']              = '--- Please Select ---';
-				
-$_['text_tax']          = 'Ex Tax:';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_sort']         = 'Sort By:';
-$_['text_default']      = 'Default';
-$_['text_name_asc']     = 'Name (A - Z)';
-$_['text_name_desc']    = 'Name (Z - A)';
-$_['text_price_asc']    = 'Price (Low &gt; High)';
-$_['text_price_desc']   = 'Price (High &gt; Low)';
-$_['text_rating_asc']   = 'Rating (Lowest)';
-$_['text_rating_desc']  = 'Rating (Highest)';
-$_['text_model_asc']    = 'Model (A - Z)';
-$_['text_model_desc']   = 'Model (Z - A)';
-$_['text_limit']        = 'Show:';
+$_['text_empty']        = 'Зараз немає спеціальних пропозицій.';
+$_['text_sale']         = 'Знижка';
+$_['text_new']          = 'Новий';
+$_['text_quick']        = 'Швидкий перегляд';
+$_['text_load_more']    = 'Завантажити ще %s товарів';
+$_['text_quantity']     = 'Кількість:';
+$_['text_manufacturer'] = 'Бренд:';
+$_['text_model']        = 'Артикул:';
+$_['text_points']       = 'Бонусні бали:';
+$_['text_price']        = 'Ціна:';
+$_['text_option']       = 'Наявні опції'; // 'Available Options' 
+$_['text_select']       = '--- Оберіть ---';
+$_['text_tax']          = 'Без ПДВ:';
+$_['text_compare']      = 'Порівняти товар (%s)';
+$_['text_sort']         = 'Сортувати за:';
+$_['text_default']      = 'За замовчанням';
+$_['text_name_asc']     = 'Ім`я (A - Я)';
+$_['text_name_desc']    = 'Ім`я (Я - A)';
+$_['text_price_asc']    = 'Ціна (Низька)';
+$_['text_price_desc']   = 'Ціна (Висока)';
+$_['text_rating_asc']   = 'Рейтинг (Низький)';
+$_['text_rating_desc']  = 'Рейтинг (Високий)';
+$_['text_model_asc']    = 'Модель (A - Я)';
+$_['text_model_desc']   = 'Модель (Я - A)';
+$_['text_limit']        = 'Показати:';
