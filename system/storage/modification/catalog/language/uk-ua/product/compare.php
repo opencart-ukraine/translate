2650ab27af0b3c40a629f5b8baca0c133a5bf5ed
ANT <velmyshanovnyi@gmail.com>
2017-06-07 19:20:11 +0300

    * 100% compare

--- system/storage/modification/catalog/language/uk-ua/product/compare.php
+++ system/storage/modification/catalog/language/uk-ua/product/compare.php
@@ -1,28 +1,30 @@
 <?php
+
+// translate for OpenCart 2.3.x by OpenCart Ukrainian Community http://opencart.ua
+// Made by ANT velmyshanovnyi@gmail.com | https://github.com/velmyshanovnyi
+// Made by ANT for https://github.com/opencart-ukraine/translate
+// version 2.3.x
+
 // Heading
-$_['heading_title']     = 'Product Comparison';
+$_['heading_title']     = 'Порівняння товарів';
 
 // Text
-$_['text_product']      = 'Product Details';
-$_['text_name']         = 'Product';
-$_['text_image']        = 'Image';
-$_['text_price']        = 'Price';
-$_['text_model']        = 'Model';
-$_['text_manufacturer'] = 'Brand';
-$_['text_availability'] = 'Availability';
-$_['text_instock']      = 'In Stock';
-$_['text_rating']       = 'Rating';
-
-				$_['text_reviews']      = '%s reviews.';
-				
-$_['text_summary']      = 'Summary';
-
-				$_['text_info']      = 'This product already added in comparison list!';
-				$_['text_warning']      = 'Maximum products to compare is 4. First added product has been removed from comparison list.';
-				
-$_['text_weight']       = 'Weight';
-$_['text_dimension']    = 'Dimensions (L x W x H)';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_success']      = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">product comparison</a>!';
-$_['text_remove']       = 'Success: You have modified your product comparison!';
-$_['text_empty']        = 'You have not chosen any products to compare.';
+$_['text_product']      = 'Деталі товарів';
+$_['text_name']         = 'Товар';
+$_['text_image']        = 'Зображення';
+$_['text_price']        = 'Ціна';
+$_['text_model']        = 'Модель';
+$_['text_manufacturer'] = 'Бренд';
+$_['text_availability'] = 'Наявність';
+$_['text_instock']      = 'В наявності';
+$_['text_rating']       = 'Рейтинг';
+$_['text_reviews']      = 'За результатами %s оглядів.';
+$_['text_summary']      = 'В загальному';
+$_['text_info']         = 'Цей продукт вже доданий до списку порівняння!'; // 'This product already added in comparison list!'
+$_['text_warning']      = 'Максимум товарів для порівняння є 4. Перший доданий товар був вилучений зі списку порівняння...'; // 'Maximum products to compare is 4. First added product has been removed from comparison list.'
+$_['text_weight']       = 'Вага';
+$_['text_dimension']    = 'Розміри (Д x Ш x В)';
+$_['text_compare']      = 'Порівняння товару (%s)';
+$_['text_success']      = 'Ви успішно додали <a href="%s">%s</a> до вашого <a href="%s"> порівняння товарів</a>!';
+$_['text_remove']       = 'Ви успішно змінили Ваше порівняння товарів!';
+$_['text_empty']        = 'Ви не обрали товарів для порівняння.';
