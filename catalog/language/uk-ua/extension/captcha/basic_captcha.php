a24f666cb45b82999454e660d3b4d8356504a871
Segnava <lsegnaval@gmail.com>
2018-02-27 11:05:07 +0200

    Переклад файлу basic_captcha.php

--- catalog/language/uk-ua/extension/captcha/basic_captcha.php
+++ catalog/language/uk-ua/extension/captcha/basic_captcha.php
@@ -3,7 +3,7 @@
 $_['text_captcha'] = 'Captcha';
 
 // Entry
-$_['entry_captcha'] = 'Enter the code in the box below';
+$_['entry_captcha'] = 'Введіть код що зображений нижче'; 
 
 // Error
-$_['error_captcha'] = 'Verification code does not match the image!';
+$_['error_captcha'] = 'Код верифікації не співпадає з картинкою!';
